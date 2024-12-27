63297dd08dbfd9557373dba20151616ac2a96d83
streamlinkworld <121393953+streamlinkworld@users.noreply.github.com>
2024-12-27 15:28:34 +0100

    Update nowm3u8.py

--- source/nowm3u8.py
+++ source/nowm3u8.py
@@ -1,6 +1,6 @@
 import requests
 
-def download_and_save_m3u8(m3u8_url, output_file):
+def download_and_save_m3u8(m3u8_url, output_file, base_url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -19,8 +19,13 @@ def download_and_save_m3u8(m3u8_url, output_file):
         response.raise_for_status()
         print(f"DEBUG: Erfolgreicher Zugriff auf {m3u8_url}")
 
+        lines = response.text.split('\n')
         with open(output_file, 'w', encoding='utf-8') as file:
-            file.writelines(f"{line.strip()}\n" for line in response.text.split('\n'))
+            for line in lines:
+                if line.startswith("#") or line.strip() == "":
+                    file.write(line + "\n")
+                else:
+                    file.write(base_url + line + "\n")
         print(f"DEBUG: M3U8-Datei wurde in {output_file} gespeichert.")
     except requests.exceptions.RequestException as e:
         print(f"ERROR: Fehler beim Herunterladen der Datei: {e}")
@@ -28,4 +33,5 @@ def download_and_save_m3u8(m3u8_url, output_file):
 if __name__ == "__main__":
     m3u8_url = "https://nowtv-live-ad.ercdn.net/nowtv/playlist.m3u8?st=fNnuSeiTM-71rJB5MFfSFg&e=1735317265"
     output_file = "result/List/NOW.m3u8"
-    download_and_save_m3u8(m3u8_url, output_file)
+    base_url = "https://nowtv.daioncdn.net/nowtv/"
+    download_and_save_m3u8(m3u8_url, output_file, base_url)
