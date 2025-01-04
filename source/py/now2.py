dfc3dc85f4c01fefa84647dc77dc8ea468bdc89f
streamlinkworld <121393953+streamlinkworld@users.noreply.github.com>
2025-01-04 15:58:51 +0100

    Create now2.py

---
+++ source/py/now2.py
@@ -0,0 +1,24 @@
+import json
+
+# JSON-Datei lesen
+with open('playlist.json', 'r') as f:
+    data = json.load(f)
+
+# M3U8-Playlist generieren
+m3u8_playlist = "#EXTM3U\n"
+m3u8_playlist += "#EXT-X-VERSION:{}\n".format(data['playlist']['#EXT-X-VERSION'])
+m3u8_playlist += "#EXT-X-INDEPENDENT-SEGMENTS\n"
+
+for stream in data['variantStreams']:
+    m3u8_playlist += "#EXT-X-STREAM-INF:PROGRAM-ID=2850,AVERAGE-BANDWIDTH={},BANDWIDTH={},RESOLUTION={}\n".format(
+        stream['#EXT-X-STREAM-INF']['AVERAGE-BANDWIDTH'],
+        stream['#EXT-X-STREAM-INF']['BANDWIDTH'],
+        stream['#EXT-X-STREAM-INF']['RESOLUTION']
+    )
+    m3u8_playlist += "{}\n".format(stream['URI'])
+
+# M3U8-Datei speichern
+with open('playlist.m3u8', 'w') as f:
+    f.write(m3u8_playlist)
+
+print("M3U8-Playlist erfolgreich generiert!")
